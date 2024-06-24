6024d188b4403fa1e30045ba0d25a4c189cd4200
SE <se@develup.pro>
2024-06-24 12:01:56 +0300

    fix serialport

--- packages/react-native-turbo-serialport/src/types/UseSerialportType.ts
+++ packages/react-native-turbo-serialport/src/types/UseSerialportType.ts
@@ -7,8 +7,24 @@ export interface UseSerialportType {
   disconnect: () => void;
   isConnected: () => Promise<boolean>;
   isServiceStarted: () => Promise<boolean>;
-  writeBytes: (message: Array<number>) => void;
-  writeString: (message: string) => void;
-  writeBase64: (message: string) => void;
-  writeHexString: (message: string) => void;
+  writeBytes: (
+    message: Array<number>,
+    deviceId?: number,
+    portInterface?: number,
+  ) => void;
+  writeString: (
+    message: string,
+    deviceId?: number,
+    portInterface?: number,
+  ) => void;
+  writeBase64: (
+    message: string,
+    deviceId?: number,
+    portInterface?: number,
+  ) => void;
+  writeHexString: (
+    message: string,
+    deviceId?: number,
+    portInterface?: number,
+  ) => void;
 }
